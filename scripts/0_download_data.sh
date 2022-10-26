3601f7058c2ab93523bc4f2e42fb07f224ee10a5
kchennen <kirsley.chennen@gmail.com>
2022-10-26 22:48:22 +0200

    Update 0_download_data.sh script

--- scripts/0_download_data.sh
+++ scripts/0_download_data.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # shellcheck disable=SC2164
-cd workflow;
+cd ../workflow;
 
 # Download genome fasta file into the genome/ folder
 wget -c -P genome/ http://ftp.ensembl.org/pub/release-107/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
@@ -10,5 +10,5 @@ wget -c -P genome/ http://ftp.ensembl.org/pub/release-107/fasta/homo_sapiens/dna
 wget -c -P annotation/ http://ftp.ensembl.org/pub/release-107/gtf/homo_sapiens/Homo_sapiens.GRCh38.107.gtf.gz
 
 ## Decompress files for use with tools
-#gunzip genome/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
-#gunzip annotation/Homo_sapiens.GRCh38.107.gtf.gz
+gunzip genome/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+gunzip annotation/Homo_sapiens.GRCh38.107.gtf.gz
